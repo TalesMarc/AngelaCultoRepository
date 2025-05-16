e6718a16855e9aaae515c63b60d0f67bea2fac67
TALES <tales@Mac-mini-de-TALES.local>
2025-05-16 16:18:22 -0300

    Atualização automática do projeto

--- node_2d.gd
+++ node_2d.gd
@@ -2,23 +2,13 @@ extends Node2D  # ou Control, dependendo de onde você estiver
 
 @onready var pause_menu = $PauseMenu
 
-func _ready():
-	process_mode = Node.ProcessModeEnum.PROCESS
-	pause_menu.visible = false  # Começa invisível
-
 func _input(event):
 	if event.is_action_pressed("Menu_Game"):
 		toggle_pause()
 
 func toggle_pause():
-	if get_tree().paused:
-		# Se já está pausado, despausa e esconde menu
-		get_tree().paused = false
-		pause_menu.visible = false
-	else:
-		# Se não está pausado, pausa e mostra menu
-		get_tree().paused = true
-		pause_menu.visible = true
+	get_tree().paused = not get_tree().paused
+	pause_menu.visible = get_tree().paused
 
 func _on_resume_button_pressed():
 	toggle_pause()
