d34732d58bccdcf52c8d934dddc1bd34e5ed0335
TALES <tales@Mac-mini-de-TALES.local>
2025-05-17 08:39:26 -0300

    Atualização manual - 2025-05-17 08:39:26

--- inicio.gd
+++ inicio.gd
@@ -1,6 +1,14 @@
 extends Control
 
 @onready var pause_menu = $PauseMenu
+@onready var fade_rect = $FadeRect
+
+func _ready():
+	# Garante que o FadeRect comece invisível
+	var c = fade_rect.modulate
+	c.a = 0.0
+	fade_rect.modulate = c
+	fade_rect.visible = true
 
 func _input(event):
 	if event.is_action_pressed("Menu_Game"):
@@ -33,4 +41,14 @@ func _on_texture_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://configuracoes.tscn")
 
 func _on_texture_button_3_pressed() -> void:
+	await fade_out()
 	get_tree().change_scene_to_file("res://node_2d.tscn")
+
+func fade_out() -> void:
+	var tween = create_tween()
+	tween.tween_property(fade_rect, "modulate:a", 1.0, 1.0)
+	await tween.finished
+
+
+func _on_sair_button_pressed() -> void:
+	get_tree().quit()
