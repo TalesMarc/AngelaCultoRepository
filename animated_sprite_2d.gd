dca146c56212105bacf6ee301aa32968cca9c814
TALES <tales@Mac-mini-de-TALES.local>
2025-05-19 15:19:40 -0300

    Atualização manual - 2025-05-19 15:19:40

--- animated_sprite_2d.gd
+++
@@ -1,9 +0,0 @@
-extends AnimatedSprite2D
-
-func _ready():
-	play("default")  # ou o nome da sua animação
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
--- configuracoes.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-var caller_node: Node = null  # Guarda o nó que chamou
-
-func voltar_on_pressed() -> void:
-	queue_free()  # Fecha esta cena
-
-	if caller_node != null:
-		caller_node.visible = true  # Reexibe o menu anterior
